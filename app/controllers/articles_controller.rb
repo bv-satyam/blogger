5b2d9a8c5fd5000aeae9f0a8aa5c618dfed90491
CyberDrudge <satyamsaxena197@gmail.com>
2020-07-09 17:17:10 +0530

    Initial Upload

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+	# http_basic_authenticate_with name: "name", password: "password", except: [:index, :show]
+
+	def index
+		@user = User.find(params[:user_id])
+    @articles = @user.articles.all
+  end
+
+	def new
+		@user = User.find(params[:user_id])
+		@article = Article.new
+  end
+
+  def edit
+  	@user = User.find(params[:user_id])
+	  @article = @user.articles.find(params[:id])
+	end
+
+  def create
+  	@user = User.find(params[:user_id])
+  	@article = @user.articles.create(article_params)
+	  if @article.save
+	    render 'show'
+	  else
+	    render 'new'
+	  end
+  end
+
+  def update
+  	@user = User.find(params[:user_id])
+	  @article = @user.articles.find(params[:id])
+	  if @article.update(article_params)
+	    render 'show'
+	  else
+	    render 'edit'
+	  end
+	end
+
+	def destroy
+		@user = User.find(params[:user_id])
+	  @article = @user.articles.find(params[:id])
+	  @article.destroy
+	  redirect_to user_articles_path
+	end
+
+	def show
+		@user = User.find(params[:user_id])
+    @article = @user.articles.find(params[:id])
+  end
+  
+  private
+	  def article_params
+	    params.require(:article).permit(:title, :text)
+	  end
+
+end
